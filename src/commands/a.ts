cff352eb8332651cd323be890a884a26cfe85802
Ked57 <kedketh@gmail.com>
2021-08-29 16:44:51 +0200

    optimise

--- src/commands/a.ts
+++ src/commands/a.ts
@@ -6,6 +6,7 @@ import {
 } from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { store } from "../app";
+import { createCarInitialMessageActionRow } from "../button";
 
 export const handleCommandA = (interaction: CommandInteraction) => {
   const carOption = interaction.options.get("voiture", true);
@@ -28,16 +29,7 @@ export const aCommand = {
       return;
     }
     const modelLabel = String(model);
-    const reactRow = new MessageActionRow().addComponents([
-      new MessageButton()
-        .setCustomId("found")
-        .setLabel("Marquer ✅")
-        .setStyle("SUCCESS"),
-      new MessageButton()
-        .setCustomId("delete")
-        .setLabel("Supprimer ❌")
-        .setStyle("DANGER"),
-    ]);
+    const reactRow = createCarInitialMessageActionRow();
     const embed = new MessageEmbed().setColor("#0099ff").setTitle(modelLabel);
     const reply = await interaction.reply({
       embeds: [embed],
